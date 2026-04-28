4bb720513c1ddef5625906edfbb589a41ea85ffc
anna koval <annakoval@gmail.com>
2026-04-28 16:03:23 +0200

    30

---
+++ Lab30/Lab30Wpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Lab30Wpf;
+
+public partial class MainWindow : Window
+{
+    private Border? _border;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void closeButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        _border = Template.FindName("newBorder", this) as Border;
+        UpdateClip();
+    }
+
+    private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        UpdateClip();
+    }
+
+    private void UpdateClip()
+    {
+        if (_border is null) return;
+        if (_border.ActualWidth <= 0 || _border.ActualHeight <= 0) return;
+
+        _border.Clip = new EllipseGeometry(new Rect(0, 0, _border.ActualWidth, _border.ActualHeight));
+    }
+}
